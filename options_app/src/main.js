479ea56383c2fdc43d91b6d0b3a00815834124db
Sergey Chernomordov <ChernomordovSV@loodsen.ru>
2023-04-10 19:48:40 +0300

    Добавление ru локализации

--- options_app/src/main.js
+++ options_app/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/ru-RU'
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/bitrix_admin.css';
 
 
 Vue.config.productionTip = false
+Vue.use(ElementUI, {locale});
 Vue.use(ElementUI);
 Vue.component("ComponentBuilder", () => import("./component-builder"));
 
