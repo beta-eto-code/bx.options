fb534382a611158e823f4a79f46079cdfcb8c34e
Sergey Chernomordov <ChernomordovSV@loodsen.ru>
2023-04-10 19:33:25 +0300

    Поправлен production путь до папки с fonts

--- options_app/vue.config.js
+++ options_app/vue.config.js
@@ -1,8 +1,17 @@
 const path = require('path')
 const { defineConfig } = require('@vue/cli-service')
 const Dotenv = require('dotenv-webpack');
+const getAssetPath = require("@vue/cli-service/lib/util/getAssetPath");
 
-console.log('ENV', process.env);
+const genAssetSubPath = dir => {
+    return getAssetPath(
+        process.env,
+        `${dir}/[name]${process.env.filenameHashing ? '.[hash:8]' : ''}[ext]`
+    )
+}
+
+const publicPath = process.env.NODE_ENV === 'production' ? '/local/components/bx.options/option.form/templates/vue_app/' : '';
+
 module.exports = defineConfig({
     transpileDependencies: true,
     outputDir: process.env.NODE_ENV === 'production'
@@ -35,6 +44,14 @@ module.exports = defineConfig({
         }
     },
     chainWebpack: config => {
-        config.optimization.delete('splitChunks')
+        config.optimization.delete('splitChunks');
+        config.module
+            .rule('fonts')
+            .test(/\.(woff2?|eot|ttf|otf)(\?.*)?$/i)
+            .set('type', 'asset')
+            .set('generator', {
+                filename: genAssetSubPath('fonts'),
+                publicPath: publicPath,
+            });
     }
 })
